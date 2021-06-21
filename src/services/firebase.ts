3d8a1cdd077d136a481b331c58003b9ab1b0b185
Erik Albuquerque <eriksilv.77@gmail.com>
2021-06-21 19:25:32 -0300

    chore: add environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIhwjI0NCJavUY7aWwaXJ-TGNfJ_P1kow",
-  authDomain: "letmeask-ac39b.firebaseapp.com",
-  databaseURL: "https://letmeask-ac39b-default-rtdb.firebaseio.com",
-  projectId: "letmeask-ac39b",
-  storageBucket: "letmeask-ac39b.appspot.com",
-  messagingSenderId: "262991099305",
-  appId: "1:262991099305:web:58692901fc2e748522c0b8"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
