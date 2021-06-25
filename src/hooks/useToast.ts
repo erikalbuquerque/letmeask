073705f7c7f610e93f2e592c6e63a156b2501c57
Erik Albuquerque <eriksilv.77@gmail.com>
2021-06-24 23:24:42 -0300

    chore:  add function handleToastPromise to useToast

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -4,8 +4,19 @@ export function useToast() {
   function handleToastSuccess(title: string) {
     toast.success(title, { position: 'top-right' })
   }
+
   function handleToastError(title: string) {
     toast.error(title, { position: 'top-right' })
   }
-  return { Toaster, handleToastSuccess, handleToastError }
+
+  function handleToastPromise(promise: Promise<void>) {
+    toast.promise(promise, {
+      loading: 'Loading',
+      success: 'Login success!',
+      error: 'Login error please try again!',
+    }, {
+      position: 'top-right'
+    });
+  }
+  return { Toaster, handleToastSuccess, handleToastError, handleToastPromise }
 }
